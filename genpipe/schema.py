d2cea6f87566538923a59f82e52be7227fa53077
kr4t0n <kyle.yang1995@gmail.com>
2025-05-08 14:21:57 +0800

    maint: update schema

--- genpipe/schema.py
+++ genpipe/schema.py
@@ -2,10 +2,10 @@ import pyarrow as pa
 
 messages_schema = pa.struct(
     [
+        pa.field("role", pa.string()),
         pa.field("content", pa.list_(pa.string())),
         pa.field("loss_mask", pa.list_(pa.float64())),
         pa.field("name", pa.string()),
-        pa.field("role", pa.string()),
     ]
 )
 
@@ -15,7 +15,7 @@ schema = pa.schema(
         pa.field("domain", pa.string()),
         pa.field("cat", pa.string()),
         pa.field("source", pa.string()),
-        pa.field("version", pa.string()),
+        pa.field("distill", pa.string()),
         pa.field("budget_tokens", pa.int64()),
     ]
 )

470bcfadf4dcd274800710abdb15cb960659ee07
kr4t0n <kyle.yang1995@gmail.com>
2025-05-08 15:48:44 +0800

    bug: fix global shuffle

--- mix.py
+++ mix.py
@@ -24,7 +24,7 @@ def main():
             data.append(temp)
             f.write(f"mix {len(temp)} data from {fname}\n")
 
-        df = dd.concat(data).sample(frac=1).repartition(npartitions=10)
+        df = dd.concat(data).repartition(npartitions=1).sample(frac=1).repartition(npartitions=10)
         f.write(f"total {len(df)} rows to {args.dst}\n")
 
     df.to_parquet(args.dst, write_index=False, schema=schema)

28f58402ed35043f3a436514e3278f0e3698d7f4
kr4t0n <kyle.yang1995@gmail.com>
2025-05-18 11:32:02 +0800

    feat: add vision schema

--- genpipe/schema.py
+++ genpipe/schema.py
@@ -19,3 +19,22 @@ schema = pa.schema(
         pa.field("budget_tokens", pa.int64()),
     ]
 )
+
+conversation_schema = pa.struct(
+    [
+        pa.field("from", pa.string()),
+        pa.field("value", pa.string()),
+    ]
+)
+
+vision_schema = pa.schema(
+    [
+        pa.field("image", pa.list_(pa.binary())),
+        pa.field("conversations", pa.list_(conversation_schema)),
+        pa.field("domain", pa.string()),
+        pa.field("cat", pa.string()),
+        pa.field("source", pa.string()),
+        pa.field("distill", pa.string()),
+        pa.field("budget_tokens", pa.int64()),
+    ]
+)
